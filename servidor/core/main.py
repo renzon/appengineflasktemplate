171f5dfa3cdae4c87a1983c0ed78cf4fea231ac9
renzon <renzon@gmail.com>
2017-07-28 12:15:53 -0300

    Arquivos estáticos e redirecionamento

--- servidor/core/main.py
+++ servidor/core/main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return redirect('/static/home.html')
--- servidor/core/tests/test_main.py
+++ servidor/core/tests/test_main.py
@@ -1,3 +1,16 @@
-def test_main_status(flask_client):
+import pytest
+
+
+@pytest.fixture
+def home_resp(flask_client):
     resp = flask_client.get('/')
-    assert 200 == resp.status_code
+    return resp
+
+
+def test_main_status(home_resp):
+    assert 302 == home_resp.status_code
+
+
+def test_main_status(home_resp):
+    path = '/'.join(home_resp.location.split('/')[-2:])
+    assert 'static/home.html' == path
